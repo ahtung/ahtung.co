4f4187245e38cfe8afca59af949acfc103373595
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-12 22:00:57 +0300

    about specs

--- spec/features/guest/about_spec.rb
+++ spec/features/guest/about_spec.rb
@@ -1,5 +1,11 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'About Page' do
-#   it 'should be visitable through home '
-# end
+require 'rails_helper'
+
+RSpec.describe 'About Page', type: :feature do
+  subject { page }
+
+  before :each do
+    visit page_path(:about)
+  end
+
+  it { should have_link('x', href: root_path) }
+end
