98ebb23790e03f2e5a3fde7e93f897d756fcb084
Dunya Kirkali <dunyakirkali@gmail.com>
2015-05-10 13:50:17 +0200

    boyscoting

--- spec/views/projects/show.html.slim_spec.rb
+++ spec/views/projects/show.html.slim_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'projects/show', type: :view do
   before(:each) do
-    @project = assign(:project, Project.create!(
-      title: 'Title',
-      content: 'MyText'
-    ))
+    @project = assign(:project, Project.create!(title: 'Title', content: 'MyText'))
   end
 
   it 'renders attributes in <p>' do
