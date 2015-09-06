23ffd0eed224a6ec41a8ebd5a53fd82ac8686674
Dunya Kirkali <dunyakirkali@gmail.com>
2015-09-06 21:45:58 +0200

    Add image to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,12 @@
 # Project
 class Project < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+
   # Validations
   validates :title, presence: true
+  validates_attachment :image, presence: true,
+    content_type: { content_type: /\Aimage\/.*\Z/ },
+    size: { in: 0..2.megabytes }
 
   # Rails admin
   rails_admin do
