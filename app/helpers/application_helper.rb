016f2c097baab2d6e2a7cbd9e8cf0a99558e3c32
Dunya Kirkali <dunyakirkali@gmail.com>
2015-09-06 21:26:16 +0200

    Add contact info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # ApplicationHelper
 module ApplicationHelper
+  def phone_number_link(text)
+    sets_of_numbers = text.scan(/[0-9]+/)
+    number = "+#{sets_of_numbers.join('-')}"
+    link_to "tel://#{number}" do
+      '<i class="material-icons">phone</i>'.html_safe
+    end
+  end
+
+  def facebook_link(page_path)
+    link_to '', "https://www.facebook.com/#{page_path}", class: 'facebook_link', target: '_blank'
+  end
 end
