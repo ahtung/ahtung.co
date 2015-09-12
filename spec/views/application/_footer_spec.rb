20e0fe18284188be61cd681f711a91ba51c59707
Dunya Kirkali <dunyakirkali@gmail.com>
2015-09-12 13:30:28 +0200

    More footer specs

--- spec/views/application/_footer_spec.rb
+++ spec/views/application/_footer_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe "application/_footer.slim", type: :view, focus: true do
     render
   end
 
+  it "should have footer" do
+    rendered.should have_selector("footer")
+  end
+  it "should have footer .footer-copyright" do
+    rendered.should have_selector("footer .footer-copyright")
+  end
+
   it "should show copyright" do
     rendered.should have_content("© 2015 All rights reserved.")
   end
