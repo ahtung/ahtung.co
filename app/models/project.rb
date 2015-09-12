bb18dbfd78175338a00125dd2c50e4a6b53ee0a6
Dunya Kirkali <dunyakirkali@gmail.com>
2015-09-12 16:32:07 +0200

    Projects#index improvements

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@
 class Project < ActiveRecord::Base
   has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
 
+
   # Validations
   validates :title, presence: true
   validates_attachment :image, presence: true, content_type: { content_type: %r{\Aimage\/.*\Z} }, size: { in: 0..2.megabytes }
