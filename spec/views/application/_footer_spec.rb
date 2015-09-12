68ebfa51993c08d85380f06cfacfc7d3fddcf2fb
Dunya Kirkali <dunyakirkali@gmail.com>
2015-09-12 14:43:26 +0200

    Improved styling

--- spec/views/application/_footer_spec.rb
+++ spec/views/application/_footer_spec.rb
@@ -1,18 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "application/_footer.slim", type: :view do
+RSpec.describe 'application/_footer.slim', type: :view do
   before :each do
     render
   end
 
-  it "should have footer" do
-    rendered.should have_selector("footer")
+  it 'should have footer' do
+    rendered.should have_selector('footer')
   end
-  it "should have footer .footer-copyright" do
-    rendered.should have_selector("footer .footer-copyright")
+  it 'should have footer .footer-copyright' do
+    rendered.should have_selector('footer .footer-copyright')
   end
 
-  it "should show copyright" do
-    rendered.should have_content("© 2015 All rights reserved.")
+  it 'should show copyright' do
+    rendered.should have_content('© 2015 All rights reserved.')
   end
 end
--- spec/views/application/_menu_spec.rb
+++ spec/views/application/_menu_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "application/_menu.slim", type: :view do
+RSpec.describe 'application/_menu.slim', type: :view do
   before :each do
     render
   end
 
-  it "should have link to work" do
+  it 'should have link to work' do
     rendered.should have_link('Work', href: projects_path)
   end
-  it "should have link to about" do
-    rendered.should have_link('About', href: page_path("about"))
+  it 'should have link to about' do
+    rendered.should have_link('About', href: page_path('about'))
   end
-  it "should have link to contact" do
-    rendered.should have_link('Contact', href: page_path("contact"))
+  it 'should have link to contact' do
+    rendered.should have_link('Contact', href: page_path('contact'))
   end
 end
