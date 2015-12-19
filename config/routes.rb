9b7e9aac6c6b61c4d1fc410a56d02b42452f6a69
Dunya Kirkali <dunyakirkali@gmail.com>
2015-12-19 13:10:07 +0100

    Spring clean up

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
   # Admin
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  # Home
-  resources :projects, only: [:index]
-  root 'projects#index'
-
   # Sitemap
   constraints(format: 'xml') do
     get '/sitemap', to: redirect('https://s3.eu-central-1.amazonaws.com/ahtung/sitemaps/sitemap.xml.gz')
