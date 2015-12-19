12a25a98aa3925dde0cf0efb7288876e0d1c7696
Dunya Kirkali <dunyakirkali@gmail.com>
2015-12-19 16:21:35 +0100

    Add placeholers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_projects
+  before_action :set_placeholders
 
   private
 
   def set_projects
     @projects = Project.all
+  end
+
+  def set_placeholders
+    con = @projects.count
+    while con % 3 != 0
+      con += 1
+    end
+    @placeholders = con - @projects.count
   end
 end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :project do
     title { Faker::Lorem.word }
-    subtitle { Faker::Lorem.sentence }
+    subtitle { Faker::Lorem.sentence(1) }
     url { Faker::Internet.url }
   end
 end
