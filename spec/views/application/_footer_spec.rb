6460d03ccb311bcf61edf9e42afd28d634744422
Dunya Kirkali <dunyakirkali@gmail.com>
2015-12-22 20:20:26 +0100

    Add locale translations Increase coverage

--- spec/views/application/_footer_spec.rb
+++ spec/views/application/_footer_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'application/_footer.slim', type: :view do
-  before :each do
-    render
-  end
+  I18n.available_locales.each do |locale|
+    context "in #{locale}" do
+      before :each do
+        I18n.locale = locale
+        render
+      end
 
-  it 'should have footer' do
-    rendered.should have_selector('footer')
-  end
-  it 'should have footer .footer-copyright' do
-    rendered.should have_selector('footer .valign-wrapper')
-  end
+      it 'should have footer' do
+        rendered.should have_selector('footer')
+      end
+      it 'should have footer .footer-copyright' do
+        rendered.should have_selector('footer .valign-wrapper')
+      end
 
-  it 'should show copyright' do
-    rendered.should have_content('We would love to hear from you © 2015 Ahtung - All rights reserved - Design by Madco')
+      it 'should show copyright' do
+        rendered.should have_content('We would love to hear from you © 2015 Ahtung - All rights reserved - Design by Madco')
+      end
+    end
   end
 end
