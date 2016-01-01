5672ab98af791bc0001957fe971c5f379b655457
Dunya Kirkali <dunyakirkali@gmail.com>
2016-01-01 13:53:12 +0200

    Correct cors

--- config/application.rb
+++ config/application.rb
@@ -33,16 +33,9 @@ module Ahtung
     config.middleware.insert_before 0, 'Rack::Cors', debug: true, logger: (-> { Rails.logger }) do
       allow do
         origins '*'
-        resource '/cors',
+        resource '/assets/*',
           headers: :any,
-          methods: [:post],
-          credentials: true,
-          max_age: 0
-
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :delete, :put, :patch, :options, :head],
-          max_age: 0
+          methods: [:get]
       end
     end
   end
