cc41bef021ed14978b8e12a9d9182b09429c80ef
Dunya Kirkali <dunyakirkali@gmail.com>
2016-01-06 16:58:57 +0200

    Configure CORS

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,10 @@ module Ahtung
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # CORS
-    config.middleware.insert_before 0, 'Rack::Cors' do
+    config.middleware.insert_before 0, 'Rack::Cors', debug: true, logger: (-> { Rails.logger }) do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :options]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head], max_age: 0
       end
     end
   end
