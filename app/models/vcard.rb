2b5d43c36cc7d0d77ce82f3e1ee2a449b989b4c3
Onur Kucukkece <onurkucukkece@gmail.com>
2016-01-31 14:37:52 +0200

    send sms

--- app/models/vcard.rb
+++ app/models/vcard.rb
@@ -7,5 +7,11 @@ class Vcard
   attr_accessor :phone_number
 
   def send_card
+    @client = Twilio::REST::Client.new ENV['TWILLO_SID'], ENV['TWILLO_TOKEN']
+    @client.account.messages.create({
+      from: '+12053154072',
+      to: phone_number,
+      body: '',
+    })
   end
 end
