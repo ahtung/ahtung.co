4061a7cb9343aac120a7e302361c33d2ed0f59c3
Dunya Kirkali <dunyakirkali@gmail.com>
2016-11-05 21:20:24 +0100

    Better migrations

--- db/migrate/20150906193234_add_image_columns_to_projects.rb
+++ db/migrate/20150906193234_add_image_columns_to_projects.rb
@@ -1,9 +1,15 @@
 class AddImageColumnsToProjects < ActiveRecord::Migration
   def up
-    add_attachment :projects, :image
+    add_column :projects, :image_file_name, :string
+    add_column :projects, :image_content_type, :string
+    add_column :projects, :image_file_size, :integer
+    add_column :projects, :image_updated_at, :datetime
   end
 
   def down
-    remove_attachment :projects, :image
+    remove_column :projects, :image_file_name, :string
+    remove_column :projects, :image_content_type, :string
+    remove_column :projects, :image_file_size, :integer
+    remove_column :projects, :image_updated_at, :datetimer
   end
 end
--- db/migrate/20151219115000_change_projects.rb
+++ db/migrate/20151219115000_change_projects.rb
@@ -3,6 +3,9 @@ class ChangeProjects < ActiveRecord::Migration
     remove_column :projects, :icon
     remove_column :projects, :color
     remove_column :projects, :content
-    remove_attachment :projects, :image
+    remove_column :projects, :image_file_name, :string
+    remove_column :projects, :image_content_type, :string
+    remove_column :projects, :image_file_size, :integer
+    remove_column :projects, :image_updated_at, :datetime
   end
 end
