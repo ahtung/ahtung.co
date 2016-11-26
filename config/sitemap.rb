d981f384ea4725222bfb1f438c8e8aed9db71388
onur <onurkucukkece@gmail.com>
2016-11-26 13:22:37 +0300

    changed sitemap host to s3

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,7 @@
 SitemapGenerator::Sitemap.default_host = 'https://www.ahtung.co'
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
-SitemapGenerator::Sitemap.sitemaps_host = ENV['CDN']
+SitemapGenerator::Sitemap.sitemaps_host = "http://s3.#{ENV['FOG_REGION']}.amazonaws.com/#{ENV['FOG_DIRECTORY']}/"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.create do
   # Root
