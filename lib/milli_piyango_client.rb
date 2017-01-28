997846974a6b37451394df78bfe2380ee17c9380
Dunya Kirkali <dunyakirkali@gmail.com>
2017-01-28 14:21:55 +0100

    Fix today check

--- lib/milli_piyango_client.rb
+++ lib/milli_piyango_client.rb
@@ -13,7 +13,7 @@ class MilliPiyangoClient
     @date = date
     result_day = "#{day}?"
     return if d_day.future?
-    return unless today.send(result_day)
+    return unless Date.today.send(result_day)
     content = open(url).read
     @result = JSON.parse(content)['data']['rakamlarNumaraSirasi']
     push
