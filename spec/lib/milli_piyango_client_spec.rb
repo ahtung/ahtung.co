d579f176b08cc0625fc472cfdfbcf8872b2dea2f
Dunya Kirkali <dunyakirkali@gmail.com>
2017-01-28 13:25:47 +0100

    Add specs for milli_piyango_client

---
+++ spec/lib/milli_piyango_client_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe MilliPiyangoClient do
+  describe 'push_results' do
+    it 'return if d_day in future'
+    it 'No results if 404'
+    it 'Push results if all green'
+  end
+end
